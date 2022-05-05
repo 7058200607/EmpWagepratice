eb5a4b0350c732df10c4cd07ed3dd6108a3f6b34
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-05 15:02:51 +0530

    employeeispresentorabsent

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWageComputation 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+        }
+    }
+}
